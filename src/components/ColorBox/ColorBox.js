52c86ae795b2faa375c6ea92857ba67ab1a6a803
Abdelrahman Soltan <asoltan@instabug.com>
2023-02-06 12:12:17 +0200

    feat: :sparkles: enable "copy to clipboard" functionality

--- src/components/ColorBox/ColorBox.js
+++ src/components/ColorBox/ColorBox.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 export default class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isCopied: false };
+  }
+  changeCopyState = () => {
+    this.setState({ isCopied: true });
+    setTimeout(() => {
+      this.setState({ isCopied: false });
+    }, 1500);
+  };
   render() {
     const { name, background } = this.props;
     return (
-      <div style={{ background }} className='ColorBox'>
-        <div className='copy-container'>
-          <div className='box-content'>
-            <span>{name}</span>
+      <CopyToClipboard text={background.toUpperCase()} onCopy={this.changeCopyState}>
+        <div style={{ background }} className='ColorBox'>
+          <div className='copy-container'>
+            <div className='box-content'>
+              <span>{name}</span>
+            </div>
+            <button className='copy-button'>Copy</button>
           </div>
-          <button className='copy-button'>Copy</button>
+          <span className='see-more'>More</span>
         </div>
-        <span className='see-more'>More</span>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
