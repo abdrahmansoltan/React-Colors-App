b459f9481eff6433ce5d5a791ad4ff497a3cc77f
Abdelrahman Soltan <asoltan@instabug.com>
2023-02-07 07:48:28 +0200

    feat: :sparkles: add color slider to control color level

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,14 +1,32 @@
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import React, { Component } from 'react';
 import ColorBox from '../ColorBox/ColorBox';
 import './Palette.css';
 
 export class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+  }
+  changeLevel = level => {
+    this.setState({ level }); // 100 -> 900 with (100 step)
+  };
   render() {
-    const colorBoxes = this.props.palette.colors[300].map(color => (
+    const colorBoxes = this.props.palette.colors[this.state.level].map(color => (
       <ColorBox background={color.hex} name={color.name} />
     ));
     return (
       <div className='Palette'>
+        <div className='slider'>
+          <Slider
+            defaultValue={this.state.level}
+            min={100}
+            max={900}
+            step={100}
+            onChange={this.changeLevel}
+          />
+        </div>
         <div className='Palette-colors'>{colorBoxes}</div>
       </div>
     );
