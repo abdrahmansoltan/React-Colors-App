925fe409fca2b8d034077baed2c45933f00dfbf0
Abdelrahman Soltan <asoltan@instabug.com>
2023-02-09 12:15:14 +0200

    refactor: :truck: move Palette-footer into separate component

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ColorBox from '../ColorBox/ColorBox';
 import Navbar from '../Navbar/Navbar';
+import PaletteFooter from '../PaletteFooter/PaletteFooter';
 import './Palette.css';
 
 export class Palette extends Component {
@@ -38,10 +39,8 @@ export class Palette extends Component {
         />
 
         <div className='Palette-colors'>{colorBoxes}</div>
-        <footer className='Palette-footer'>
-          {paletteName}
-          <span className='emoji'>{emoji}</span>
-        </footer>
+
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
